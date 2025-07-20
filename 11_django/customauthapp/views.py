26dfb1df42b5749e468aa01c42be3d7ee948d9d6
itsmeShishir <91515501+itsmeShishir@users.noreply.github.com>
2025-07-20 09:06:56 +0545

    just basic one

--- 11_django/customauthapp/views.py
+++ 11_django/customauthapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def logouts(request):
+    return render(request, "index.html")
+
